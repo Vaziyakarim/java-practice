ef4ed7016d40b0fd708c9db79e48e0b6a5a9a825
Vaziyakarim <karimvaziya545@gmail.com>
2025-09-01 20:46:19 +0530

    Taking input

---
+++ input.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class input {
+    public static void main(String[] args){
+        System.out.println("HII");
+        Scanner sc=new Scanner(System.in);
+             System.out.println("please enter your name:");
+            String name=sc.nextLine();
+            System.out.println("Name:"+name);
+             System.out.println("please enter your age:");
+            int age=sc.nextInt();
+            System.out.println("Age"+age);
+        }
+    }
+    
+
