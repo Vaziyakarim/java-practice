c47e6739a67e0bf4b6758a80272f26c790e0c75b
Vaziyakarim <karimvaziya545@gmail.com>
2025-09-06 23:46:18 +0530

    Ternary operator
    Ternary operator

---
+++ ternary.java
@@ -0,0 +1,8 @@
+public class ternary {
+    public static void main(String[] args) {
+       int age=20;
+    String result = (age>=18) ?"Adult": "Not Adult";
+    System.out.println(result);
+}  
+    }
+   
