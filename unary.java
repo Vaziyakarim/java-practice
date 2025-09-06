4c6f890d35147c0ab8cc994f7276c149ca974096
Vaziyakarim <karimvaziya545@gmail.com>
2025-09-06 23:45:48 +0530

    Unary Operator
    Unary Operator

---
+++ unary.java
@@ -0,0 +1,20 @@
+public class unary {
+    public static void main(String[] args) {
+        //unary
+        //increment
+        //decrement
+        int num=10;
+        num += 2;
+        System.out.println(num);
+        num++;
+         System.out.println(num);
+        num--;
+         System.out.println(num);
+
+        ++num;
+         System.out.println(num);
+        --num; 
+        System.out.println(num);
+
+    }
+}
