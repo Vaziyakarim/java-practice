9468e7d8b7e61ab886f7ffca3b8145e3eafd4664
Vaziyakarim <karimvaziya545@gmail.com>
2025-09-06 23:44:00 +0530

    Assignment operator
    Assignment operator

---
+++ assignment.java
@@ -0,0 +1,26 @@
+public class assignment {
+    public static void main(String[] args) {
+         int num1=1;
+         num1 +=5;
+         
+        int num2 =1;
+        num2 -= 5;
+
+        int num3=3;
+        num3 *= 3;
+       
+        int num4=5;
+        num4 /= 6;
+
+        int num5=4;
+        num5 %= 3;
+        
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(num4);
+        System.out.println(num5);
+       
+        
+    }
+}
