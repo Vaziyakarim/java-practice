53b59b2f29e107ff19950c59a243337ebaedcad0
Vaziyakarim <karimvaziya545@gmail.com>
2025-09-06 23:44:29 +0530

    Logical operator
    Logical operator

---
+++ logic.java
@@ -0,0 +1,13 @@
+public class logic {
+    public static void main(String[] args) {
+        //Logical Operator
+        // &&
+        // ||
+        //!
+        int age=18;
+        System.out.println(age>=10 && age<=20);
+        System.out.println(age>18 || age<20);
+       System.out.println(!(age==18));
+    } 
+    
+}
