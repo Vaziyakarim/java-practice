ab27b16455905813b647a897de1d1a01ecebdc9f
Vaziyakarim <karimvaziya545@gmail.com>
2025-09-08 20:11:25 +0530

    Printing sum of 2 no's using function
    Printing sum of 2 no's using function

---
+++ funAdd.java
@@ -0,0 +1,13 @@
+public class funAdd {
+    public static int addNum( int a,int b){
+        return a+b;
+    }
+    
+    
+    public static void main(String[] args) {
+        int sum=addNum(5,10);
+        System.out.println("sum: " +sum);
+        
+    }
+
+}
