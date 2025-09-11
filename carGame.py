7c308dd3e1e89ed91915bb83e4c773f822284393
Vaziyakarim <karimvaziya545@gmail.com>
2025-09-11 21:36:03 +0530

    CarGame
    CarGame

--- carGame.py
+++ carGame.py
@@ -1,30 +1,49 @@
-command=""
-started = False
-while True:
-    
-   command=input(">").lower()
-   if command == "start":
-        if started:
-            print("Car is already started")
-        else:
-            started = True
-            print("Car started...")
-            
-   elif command == "stop":
-        if not started:
-            print("Car is already stopped")
-        else:
-            started = False
-            print("Car stopped")
-            
-   elif command == "help":
-        print("""
-start - to start the car
-stop - to stop the car
-quit - to exit""")
-            
-   elif command == "quit":
-        break
-        
-   else:
-        print("I don't understand...")
+import java.util.Scanner;
+
+public class CarGame {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean started = false;
+        String command = "";
+
+        while (true) {
+            System.out.print("> ");
+            command = scanner.nextLine().toLowerCase();
+
+            switch (command) {
+                case "start":
+                    if (started) {
+                        System.out.println("Car is already started");
+                    } else {
+                        started = true;
+                        System.out.println("Car started...");
+                    }
+                    break;
+
+                case "stop":
+                    if (!started) {
+                        System.out.println("Car is already stopped");
+                    } else {
+                        started = false;
+                        System.out.println("Car stopped");
+                    }
+                    break;
+
+                case "help":
+                    System.out.println("start - to start the car");
+                    System.out.println("stop - to stop the car");
+                    System.out.println("quit - to exit");
+                    break;
+
+                case "quit":
+                    System.out.println("Exiting...");
+                    scanner.close();
+                    return;
+
+                default:
+                    System.out.println("I don't understand...");
+                    break;
+            }
+        }
+    }
+}
